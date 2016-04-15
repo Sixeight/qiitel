8eaaaef6ff5eb6ae5818eddf621d7e5dd385290e
Tomohiro Nishimura <tomohiro68@gmail.com>
2016-04-15 23:58:40 +0900

    Userモデル追加

---
+++ model/user.rb
@@ -0,0 +1,12 @@
+
+require 'bundler'
+Bundler.require
+
+class User < ActiveRecord::Base
+  validates :twitter_id, uniqueness: true, presence: true
+  validates :secret,     uniqueness: true, presence: true
+
+  validates :name,      length: { maximum: 15, minimum: 1 }, presence: true
+  validates :url,       format: { with: %r!\Ahttps?://! }
+  validates :image_url, format: { with: %r!\Ahttps?://.+\.(png|jpg|jpeg|gif|svg)\z! }
+end
