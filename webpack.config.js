72164f6733672b3d0a5b9d10ab8b5e0f07ebae01
Tomohiro Nishimura <tomohiro68@gmail.com>
2017-10-08 22:04:19 +0900

    use resolve instead of join

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     entry: './src/js/main.jsx',
     output: {
-        path: path.join(__dirname, 'static/js'),
+        path: path.resolve(__dirname, 'static/js'),
         filename: 'main.js'
     },
     module: {
