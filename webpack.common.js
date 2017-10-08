23c29dd5529b504b70bd09562a9b6035cd409dbd
Tomohiro Nishimura <tomohiro68@gmail.com>
2017-10-09 02:34:45 +0900

    Lintする

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,12 @@ module.exports = {
     module: {
         rules: [
             {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: {
