9d15a832bc39de3528e3c5233e78721dc74a2731
Tomohiro Nishimura <tomohiro68@gmail.com>
2017-10-14 22:25:19 +0900

    Fix production crash

--- db/setup.rb
+++ db/setup.rb
@@ -6,12 +6,14 @@ require 'yaml'
 settings = YAML.load_file File.expand_path('../config/database.yml', __dir__)
 environment = ENV['RACK_ENV'] || 'development'
 
-# Hack for Daemonize
-# デーモン化するときに / に移動するので相対パスで指定していると誤ったファイルをみてしまう
-# http://www.rubydoc.info/gems/daemons/1.1.9/Daemons
-project_root = File.expand_path('..', __dir__)
-absolute_database_path = File.expand_path(settings[environment]['database'], project_root)
-settings[environment]['database'] = absolute_database_path
+if environment == 'development' || environment == 'test'
+  # Hack for Daemonize
+  # デーモン化するときに / に移動するので相対パスで指定していると誤ったファイルをみてしまう
+  # http://www.rubydoc.info/gems/daemons/1.1.9/Daemons
+  project_root = File.expand_path('..', __dir__)
+  absolute_database_path = File.expand_path(settings[environment]['database'], project_root)
+  settings[environment]['database'] = absolute_database_path
+end
 
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.configurations = settings
